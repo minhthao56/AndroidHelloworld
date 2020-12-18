2740925b28fff92052a21cb5d01f59b271c7a772
minthao56 <minthao5648@gmail.com>
2020-12-18 13:48:54 +0700

    add scanswifi

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,21 +1,24 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity{
-    private EditText txtEmail;
-    private  EditText txtPassword;
+
     private Button  btnLogin;
     private WifiManager wifiManager;
+    private final int MY_PERMISSIONS_ACCESS_COARSE_LOCATION = 1;
 
 
     @Override
@@ -23,16 +26,28 @@ public class MainActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtEmail = (EditText) findViewById(R.id.txtEmail);
-        txtPassword = (EditText) findViewById(R.id.txtPassword);
         btnLogin = (Button) findViewById(R.id.btnLogin);
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (!wifiManager.isWifiEnabled()) {
+            Toast.makeText(getApplicationContext(), "Turning WiFi ON...", Toast.LENGTH_LONG).show();
+            wifiManager.setWifiEnabled(true);
+        }else {
 
+            Toast.makeText(getApplicationContext(), "Turned on", Toast.LENGTH_LONG).show();
+
+        }
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             String email = txtEmail.getText().toString().trim();
-           String password = txtPassword.getText().toString();
-
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_ACCESS_COARSE_LOCATION);
+                    Toast.makeText(getApplicationContext(), "ACCESS_COARSE_LOCATION", Toast.LENGTH_LONG).show();
+                } else {
+                    wifiManager.startScan();
+                    Toast.makeText(getApplicationContext(), "Scan wifi...", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
